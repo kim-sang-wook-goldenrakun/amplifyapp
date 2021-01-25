626dee07d747143bc4185a7b5b52e335fd9a2fe2
kim-sang-wook-goldenrakun <sw.kim@goldenrakun.com>
2021-01-25 21:39:39 +0900

    v4 commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import login from './img/login.PNG'
+import loginsvg from './login.svg'
+import svgimg1 from './svgimg1.svg'
+import svgimg2 from './img/svgimg1.svg'
+
 import './App.css';
 
 function App() {
@@ -8,9 +12,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo"/>
         <img src={login} className="App-login"/>
+        <img src={loginsvg}/>
+        <img src={svgimg1}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src={svgimg2}/>
         <a
           className="App-link"
           href="https://reactjs.org"
