cf073a2f275f82af9d5ff3ce5813ec3ae1183ab5
kim-sang-wook-goldenrakun <sw.kim@goldenrakun.com>
2021-01-25 21:25:23 +0900

    v3 commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={login}/>
+        <img src={logo} className="App-logo"/>
+        <img src={login} className="App-login"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
